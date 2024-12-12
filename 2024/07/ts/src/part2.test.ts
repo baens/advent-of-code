9e6eb1e5e18b5fdd454a8727ef895d787808299c
Erik Lindblom <erik.lindblom@ww.com>
2024-12-12 15:51:49 -0700

    Day 7 part 2 WIP

--- 2024/07/ts/src/part2.test.ts
+++ 2024/07/ts/src/part2.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from "vitest";
 import { run } from "./part2";
 
 test("run returns expected", async () => {
-	expect(await run("./test.txt")).toEqual(0);
+	expect(await run("./test.txt")).toEqual(11387);
 });
