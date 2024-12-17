69d9cfa187a597e1db012a7223e1594f85758e65
Erik Lindblom <erik@baens.net>
2024-12-17 15:10:37 +0000

    WIP day 9 part 2

--- 2024/09/ts/src/part2.ts
+++ 2024/09/ts/src/part2.ts
@@ -1,3 +1,45 @@
+import fs from "node:fs";
+
 export async function run(input: string): Promise<number> {
+	const diskConfiguration = fs
+		.readFileSync(input, "utf-8")
+		.split("")
+		.map((s) => Number.parseInt(s));
+
+	const diskArray: Array<number | string> = [];
+	let currentPosition = 0;
+	const fileLengthLookup: number[] = [];
+	for (let i = 0; i < diskConfiguration.length / 2; i += 1) {
+		const length = diskConfiguration[i * 2];
+		const freeSpace = diskConfiguration[i * 2 + 1];
+		fileLengthLookup[i] = length;
+		for (let j = 0; j < length; j += 1, currentPosition += 1) {
+			diskArray[currentPosition] = i;
+		}
+
+		for (let j = 0; j < freeSpace; j += 1, currentPosition += 1) {
+			diskArray[currentPosition] = ".";
+		}
+	}
+
+	let lastSearchedPosition = 0;
+	for (let i = diskArray.length - 1; i >= 0; i -= 1) {
+		const item = diskArray[i];
+
+		if (typeof item === "string") {
+			continue;
+		}
+
+		const length = fileLengthLookup[item];
+
+		let startOfFreeSpace: number = null;
+		for (let j = 0; j < i; j += 1) {
+			if (diskArray[j] !== ".") {
+				startOfFreeSpace = null;
+				continue;
+			}
+		}
+	}
+
 	return 0;
 }
